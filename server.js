e2ca4b436b1b9670de8b386151ad65bf38e7cbfc
harshvardhan singh <harshvardhansingh458@protonmail.com>
2024-02-27 00:33:42 +0530

    added move event handler

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const { Server } = require("socket.io");
 const {
     readyToPlay
 } = require("./event_handlers/User.event_handlers");
+const {
+    move
+} = require("./event_handlers/Game.event_handlers");
 
 const io = new Server({
     cors: {
@@ -12,6 +15,9 @@ const io = new Server({
 io.on("connection", (socket) => {
     // user events
     socket.on("ready_to_play", data => readyToPlay(socket, data));
+
+    // game events
+    socket.on("move", data => move(socket, data));
 });
 
 io.listen(5000);
