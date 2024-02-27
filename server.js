e6d85583f1b4505376ed0a865564ec4e9810f287
harshvardhan singh <harshvardhansingh458@protonmail.com>
2024-02-27 14:01:12 +0530

    registered disconnect event

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { Server } = require("socket.io");
 const {
-    readyToPlay
+    readyToPlay,
+    onDisconnect
 } = require("./event_handlers/User.event_handlers");
 const {
     move
@@ -15,6 +16,7 @@ const io = new Server({
 io.on("connection", (socket) => {
     // user events
     socket.on("ready_to_play", data => readyToPlay(socket, data));
+    socket.on("disconnect", () => onDisconnect(socket));
 
     // game events
     socket.on("move", data => move(socket, data));
